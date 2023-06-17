b0f5d36140dd5b68aba39b2e42239557ab2f3d72
Wasis Sasoko - AIR <yokowasis@gmail.com>
2023-06-17 14:13:51 +0800

    iinit

---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+ *
+ * @param {string} key
+ * @returns {any}
+ */
+const getState = async (key) => {
+  const res = await fetch("https://bima-global.bimasoft.workers.dev/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      key,
+    }),
+  });
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+/**
+ *
+ * @param {string} key
+ * @param {any} value
+ */
+const setState = async (key, value) => {
+  const res = await fetch("https://bima-global.bimasoft.workers.dev/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      key,
+      value,
+    }),
+  });
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export { getState, setState };
